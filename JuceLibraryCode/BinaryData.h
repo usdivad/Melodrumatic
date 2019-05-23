3c611ee59306aae87b77a13e41f4c7c5ec00ad2e
David Su <david.d.su@gmail.com>
2019-05-22 22:27:42 -0400

    add custom fonts

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,42 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   gs_bold_ttf;
+    const int            gs_bold_ttfSize = 62492;
+
+    extern const char*   gs_italic_ttf;
+    const int            gs_italic_ttfSize = 78304;
+
+    extern const char*   gs_light_ttf;
+    const int            gs_light_ttfSize = 99540;
+
+    extern const char*   gs_regular_ttf;
+    const int            gs_regular_ttfSize = 71788;
+
+    extern const char*   pcd_ttf;
+    const int            pcd_ttfSize = 15044;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 5;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
