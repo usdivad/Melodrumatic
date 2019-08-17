bab321062a82ab517542f4ed80fe85f1a8d77da3
David Su <david.d.su@gmail.com>
2019-08-17 11:15:16 -0700

    update version number

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -42,7 +42,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "Melodrumatic";
     const char* const  companyName    = "David Su";
-    const char* const  versionString  = "0.1.1";
-    const int          versionNumber  = 0x101;
+    const char* const  versionString  = "0.1.2";
+    const int          versionNumber  = 0x102;
 }
 #endif
